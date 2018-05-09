bcc2f5a9fc7bc8d9672bc5c33237ab787673101b
Hannah von Reth <vonreth@kde.org>
2018-05-09 10:17:19 +0200

    Fix imported target for Unix

--- AnalyzerSDKConfig.cmake
+++ AnalyzerSDKConfig.cmake
@@ -1,17 +1,23 @@
-if (APPLE)
-    set(AnalyzerSDK_IMPORTLIB libAnalyzer.dylib)
-elseif(MSVC)
-    if( CMAKE_CL_64 )
-        set(AnalyzerSDK_IMPORTLIB Analyzer64.lib)
-    else()
-         set(AnalyzerSDK_IMPORTLIB Analyzer.lib)
-    endif()
-elseif(UNIX AND NOT APPLE)
-    set(AnalyzerSDK_IMPORTLIB libAnalyzer.so)
+add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
+set_target_properties(Saleae::AnalyzerSDK PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
+
+if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
+    set(SUFFIX)
+else()
+    set(SUFFIX "64")
 endif()
 
-
-add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
-
-set_property(TARGET Saleae::AnalyzerSDK PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
-set_property(TARGET Saleae::AnalyzerSDK PROPERTY IMPORTED_IMPLIB ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB})
+if (APPLE)
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/libAnalyzer.dylib)
+elseif(WIN32)
+    set(AnalyzerSDK_IMPORTLIB Analyzer${SUFFIX})
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.dll
+                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.lib)
+elseif(UNIX AND NOT APPLE)
+    set(AnalyzerSDK_IMPORTLIB libAnalyzer${SUFFIX})
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.so
+                                              IMPORTED_SONAME   ${AnalyzerSDK_IMPORTLIB}.so)
+endif()
