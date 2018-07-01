453ac3c5b8a75a7049796df7a5b08453167e84ab
Marcus10110 <markgarrison@saleae.com>
2018-07-01 15:40:05 -0700

    added cmake lists and readme to the legacy branch.

---
+++ AnalyzerSDKConfig.cmake
@@ -0,0 +1,23 @@
+add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
+set_target_properties(Saleae::AnalyzerSDK PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
+
+if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
+    set(SUFFIX)
+else()
+    set(SUFFIX "64")
+endif()
+
+if (APPLE)
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/libAnalyzer.dylib)
+elseif(WIN32)
+    set(AnalyzerSDK_IMPORTLIB Analyzer)
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.dll
+                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.lib)
+elseif(UNIX AND NOT APPLE)
+    set(AnalyzerSDK_IMPORTLIB libAnalyzer${SUFFIX})
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/${AnalyzerSDK_IMPORTLIB}.so
+                                              IMPORTED_SONAME   ${AnalyzerSDK_IMPORTLIB}.so)
+endif()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Saleae Analyzer SDK Legacy Branch
+
+This branch, “1.1.14-legacy”, contains the libraries and headers for an older version of the Analyzer SDK, used exclusively for debugging analyzers on Windows targets. Production analyzers should be built using the master branch of this repository. Debugging custom analyzers on MacOS or Linux operating systems should also be done using the master branch.
+
+The latest Saleae software should not be used with analyzers built with this SDK. We recommend using the 32 bit Windows release of the 1.1.18 Logic Software specifically when debugging analyzers on Windows.
