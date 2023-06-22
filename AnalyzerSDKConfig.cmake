6f5dcc61bacd148e9ee9c45bb53a6635b89d90f9
Marcus10110 <markgarrison@saleae.com>
2023-06-22 16:47:53 -0700

    switched to only loading a single library, and using CMAKE_OSX_ARCHITECTURES to detect cross builds.

--- AnalyzerSDKConfig.cmake
+++ AnalyzerSDKConfig.cmake
@@ -1,27 +1,31 @@
+add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
+set_target_properties(Saleae::AnalyzerSDK PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
+
 if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
     message(FATAL_ERROR "Analyzer SDK only supports 64 bit builds")
 endif()
 
 if (APPLE)
-add_library(Saleae::AnalyzerSDK INTERFACE IMPORTED)
-    # add both the x86_64 and arm64 versions of the library.
-    add_library(Saleae::AnalyzerSDK_x64 SHARED IMPORTED)
-    set_target_properties(Saleae::AnalyzerSDK_x64 PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/libAnalyzer.dylib)
-    add_library(Saleae::AnalyzerSDK_arm64 SHARED IMPORTED)
-    set_target_properties(Saleae::AnalyzerSDK_arm64 PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_arm64/libAnalyzer.dylib)
-    set_property(TARGET Saleae::AnalyzerSDK PROPERTY INTERFACE_LINK_LIBRARIES Saleae::AnalyzerSDK_x64 Saleae::AnalyzerSDK_arm64)
+    if("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "arm64")
+        set(LIB_DIR "lib_arm64")
+    elseif("${CMAKE_OSX_ARCHITECTURES}" STREQUAL "x86_64")
+        set(LIB_DIR "lib_x86_64")
+    elseif(CMAKE_OSX_ARCHITECTURES STREQUAL "")
+        message("CMAKE_OSX_ARCHITECTURES not set, defaulting to system processor")
+        set(LIB_DIR "lib_${CMAKE_SYSTEM_PROCESSOR}")
+    else()
+        message(FATAL_ERROR "Universal Binaries are not supported. Either set CMAKE_OSX_ARCHITECTURES to a single supported platform, or do not set it.")
+    endif()
+
+    message("LIB_DIR: ${LIB_DIR}")
+    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/${LIB_DIR}/libAnalyzer.dylib)
 elseif(WIN32)
-add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
     set_target_properties(Saleae::AnalyzerSDK PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/Analyzer.dll
-                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib_x64/Analyzer.lib)
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x86_64/Analyzer.dll
+                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib_x86_64/Analyzer.lib)
 elseif(UNIX AND NOT APPLE)
-add_library(Saleae::AnalyzerSDK SHARED IMPORTED)
     set_target_properties(Saleae::AnalyzerSDK PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/libAnalyzer.so
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x86_64/libAnalyzer.so
                                               IMPORTED_SONAME   libAnalyzer.so)
 endif()
-
-set_target_properties(Saleae::AnalyzerSDK PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${CMAKE_CURRENT_LIST_DIR}/include)
