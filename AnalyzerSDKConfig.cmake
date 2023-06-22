e2758704437dfcf5f20d5f64880811f59f1483b8
Marcus10110 <markgarrison@saleae.com>
2023-06-21 17:44:27 -0700

    added new arm64 temporary build for macOS. reorganized libs directories. untested.

--- AnalyzerSDKConfig.cmake
+++ AnalyzerSDKConfig.cmake
@@ -6,14 +6,19 @@ if(CMAKE_SIZEOF_VOID_P STREQUAL "4")
 endif()
 
 if (APPLE)
-    set_target_properties(Saleae::AnalyzerSDK PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/libAnalyzer.dylib)
+    add_library(Saleae::AnalyzerSDK_x64 SHARED IMPORTED)
+    set_target_properties(Saleae::AnalyzerSDK_x64 PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/libAnalyzer.dylib)
+    add_library(Saleae::AnalyzerSDK_am64 SHARED IMPORTED)
+    set_target_properties(Saleae::AnalyzerSDK_x64 PROPERTIES
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_arm64/libAnalyzer.dylib)
+    set_property(TARGET Saleae::AnalyzerSDK PROPERTY INTERFACE_LINK_LIBRARIES Saleae::AnalyzerSDK_x64 Saleae::AnalyzerSDK_arm64)
 elseif(WIN32)
     set_target_properties(Saleae::AnalyzerSDK PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/Analyzer.dll
-                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib/Analyzer.lib)
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/Analyzer.dll
+                                              IMPORTED_IMPLIB   ${CMAKE_CURRENT_LIST_DIR}/lib_x64/Analyzer.lib)
 elseif(UNIX AND NOT APPLE)
     set_target_properties(Saleae::AnalyzerSDK PROPERTIES
-                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib/libAnalyzer.so
+                                              IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/lib_x64/libAnalyzer.so
                                               IMPORTED_SONAME   libAnalyzer.so)
 endif()
